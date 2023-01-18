c9a80a903fb1898ae0c5686cb268469d8bb9ca69
sebastian_andreoletti <andreole@ar.ibm.com>
2023-01-18 11:58:29 -0300

    pend

--- src/index.js
+++ src/index.js
@@ -5,6 +5,36 @@ import fs            from "fs" ;
 import path          from "path" ;
 import PDFDoc        from "pdfkit" ;
 //
+const replaceTextLines = (argArrayTemplate,argData) => {
+    try {
+        //
+        let outArray = [] ;
+        let __DATA__ = {} ;
+        //
+        for ( let keyData in argData ){
+            __DATA__[keyData] = argData[keyData] ;
+        } ;
+        //
+        for ( let posL=0; posL<argArrayTemplate.length; posL++ ){
+            let newObj  = argArrayTemplate[posL] ;
+            let newText = ""+String(newObj.text) ;
+            if ( newText.indexOf("${")>-1 ){
+                let tempText = newText.replace(/\${/g,"${__DATA__.") ;
+                // console.log("...(A) tempText: ",tempText,";") ;
+                tempText     = eval( "`"+tempText+"`" ) ;
+                newObj.text  = tempText ;
+                // console.log("...(B) tempText: ",tempText,";") ;
+            } ;
+            outArray.push(newObj) ;
+        } ;
+        //
+        return outArray ;
+        //
+    } catch(errRTL){
+        throw errRTL ;
+    } ;
+} ;
+//
 export const generatePDF = (argOpt) => {
     return new Promise((respOk,respRech)=>{
         try {
@@ -12,32 +42,29 @@ export const generatePDF = (argOpt) => {
             let pathPDF = path.join( argOpt.path ? argOpt.path :  __dirname, "../test/output.pdf" ) ;
             console.log("..pathPDF: ",pathPDF,";") ;
             //
-            let doc = new PDFDoc({
-                layout: 'portrait',
-                pdfVersion: "1.4",
-                margin: 1,
-                size: 'A4'
-            }) ;
+            const { data } = argOpt ;
+            const { general, defaults, fixedLines } = argOpt.template ;
+            //
+            let doc = new PDFDoc({layout: general.orientation, pdfVersion: general.pdfVersion||"1.4", margin: general.margin||1, size: general.size||'A4' }) ;
+            //
             doc.pipe(fs.createWriteStream( pathPDF ));
             //
-            console.log("...data.length: ",argOpt.data.length,";");
-            for ( let posDD=0; posDD<argOpt.data.length; posDD++ ){
-                let elemText = argOpt.data[posDD] ;
-                if ( elemText["#text"]!=undefined && elemText["#text"].length>0 ){
-                    // doc.switchToPage(0) ;
-                    // doc .font("Courier-Bold")
-                    let fontNN = elemText.attr.fontName==undefined ? "Courier-Bold" : elemText.attr.fontName ;
-                    fontNN     = fontNN.replace(/_/g,"-") ;
-                    switch(fontNN){
-                        case "Helvetica-bold": fontNN="Helvetica-Bold" ; break ;
-                        default: break ;
-                    } ;
-                    console.log("...fontNN: ",fontNN," fSize: ",elemText.attr.fontSize," x: ",elemText.attr.x," y: ",elemText.attr.y," text: ",elemText["#text"],"") ;
-                    //
-                    doc .font( fontNN )
-                        .fontSize( elemText.attr.fontSize )
-                        .text( ""+String(elemText["#text"]) , parseFloat(elemText.attr.x)* 2.54 , parseFloat(elemText.attr.y)* 2.54  ) ;
+            let arrayLines = replaceTextLines( fixedLines, data ) ;
+            for ( let posDD=0; posDD<arrayLines.length; posDD++ ){
+                let elemText = arrayLines[posDD] ;
+                //
+                let fontSize = elemText.fontSize==undefined ? defaults.fontSize||9 : elemText.fontSize ;
+                let fontNN   = elemText.fontName==undefined ? defaults.fontName||"Courier-Bold" : elemText.fontName ;
+                fontNN     = fontNN.replace(/_/g,"-") ;
+                switch(fontNN){
+                    case "Helvetica-bold": fontNN="Helvetica-Bold" ; break ;
+                    default: break ;
                 } ;
+                //
+                doc .font( fontNN )
+                    .fontSize( fontSize )
+                    .text( ""+elemText.text , parseFloat(elemText.x)* 2.54 , parseFloat(elemText.y)* 2.54  ) ;
+                //
             } ;
             //
             doc.end() ;
--- test/genpdf.js
+++ test/genpdf.js
@@ -3,11 +3,11 @@
 */
 import fs                            from "fs" ;
 import path                          from "path" ;
-import xmlParser                     from "fast-xml-parser" ;
 import { generatePDF }               from "../src/index" ;
 //
+/*
+import xmlParser                     from "fast-xml-parser" ;
 let xmlStr = fs.readFileSync( path.join(__dirname,"./imagen.xml"), {encoding:"utf8"} ) ;
-// console.log("..xmlStr: ",xmlStr,";") ;
 const options = {
     attributeNamePrefix : "",
     attrNodeName: "attr", //default is 'false'
@@ -21,16 +21,14 @@ const options = {
     cdataTagName: "__cdata", //default is 'false'
     cdataPositionChar: "\\c",
     parseTrueNumberOnly: false,
-    arrayMode: false, //"strict"
-    // attrValueProcessor: (val, attrName) => he.decode(val, {isAttributeValue: true}),//default is a=>a
-    // tagValueProcessor : (val, tagName) => he.decode(val), //default is a=>a
+    arrayMode: false, 
     stopNodes: ["parse-me-as-string"]
 } ;
 let tObj       = xmlParser.getTraversalObj(xmlStr,options);
 let jsonParsed = xmlParser.convertToJson( tObj, options ) ;
 console.log("\n\n....jsonParsed: ",jsonParsed,";") ;
-//
 let arrayText = jsonParsed['iolc:Invoices']['iolc:Invoice']['iolc:InvoiceHeader']['iolc:Text'] ;
+*/
 /*
 arrayText.forEach((elemText)=>{
     console.log("...elemText: attr: ",elemText.attr," tt: ",elemText["#text"],";") ;
@@ -38,7 +36,10 @@ arrayText.forEach((elemText)=>{
 */
 // console.log("\n....header: ",jsonParsed['iolc:Invoices']['iolc:Invoice']['iolc:InvoiceHeader'],";") ;
 //
-generatePDF({ path: __dirname, data: arrayText })
+let tempDataInvoice = JSON.parse( fs.readFileSync( path.join(__dirname,"./invoice.json"),"utf-8") );
+let tempTemplate    = JSON.parse( fs.readFileSync( path.join(__dirname,"./templateInvoice.json"),"utf-8") );
+//
+generatePDF({ path: __dirname, data: tempDataInvoice , template: tempTemplate })
     .then((rr)=>{
         console.log("...r: ",rr,";") ;
     })
